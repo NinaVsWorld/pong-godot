96d3a92f43fd23cedc0c294fac93709886e67ae1
NinaVsWorld <130551356+NinaVsWorld@users.noreply.github.com>
2026-09-13 21:10:56 +1000

    programmed end menu

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 @onready var game_manager = %GameManager
 @onready var ball = $GameObjects/ball
-@onready var canvas_layer = $CanvasLayer
+@onready var end_menu = $EndMenu
 
 func _ready():
 	ball.point_scored.connect(scored)
-	canvas_layer.hide()
+	end_menu.hide()
 
 func scored(paddle : String):
 	if paddle == "paddle1":
@@ -14,11 +14,18 @@ func scored(paddle : String):
 	if paddle == "paddle2":
 		game_manager.add_point2()
 		
-
 func _process(delta):
 	game_over()
 	
 func game_over():
-	if game_manager.get_player1_score() >= 15 or game_manager.get_player2_score() >= 15:
+	if game_manager.get_player1_score() >= 1 or game_manager.get_player2_score() >= 15:
 		get_tree().paused = true
-		canvas_layer.show()
+		end_menu.show()
+
+func _on_restart_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func _on_home_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
