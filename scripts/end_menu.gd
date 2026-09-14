d89c2946add3039a1cb064463c44cc1a3049906a
NinaVsWorld <130551356+NinaVsWorld@users.noreply.github.com>
2026-09-15 00:04:42 +1000

    project overhaul

--- scripts/end_menu.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _on_restart_pressed():
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
-
-func _on_home_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
